af74e300b15b25f7354bbce279e3279e241397e7
Ivan <i.ohapkin@gmail.com>
2022-11-05 16:41:38 +0700

    Add /api/fileanalyse method

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
+
+const multer  = require('multer')
+const upload = multer({ dest: 'public/uploads/' })
+
 require('dotenv').config()
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  
+  const file = req.file
 
-
+  res.send({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+  
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
